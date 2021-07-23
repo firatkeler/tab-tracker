15e81edf1b552e6c95fc987846dec807e298b04a
firatkeler <firatkeler@gmail.com>
2021-07-23 10:41:39 +0300

    vuetify installation

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,14 +1,41 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" v-model="email" name="email" placeholder="email" />
-    <br>
-    <input type="password" v-model="password" name="password" placeholder="password" />
-    <br>
-    <div class="error" v-html="error"></div>
-    <br>
-    <button @click="register">Register</button>
-  </div>
+  <v-layout column align-center justify-center>
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+          >
+            <div class="white elevation-2">
+              <v-toolbar flat dense class="cyan" dark>
+                <v-toolbar-title>Register</v-toolbar-title>
+              </v-toolbar>
+              <v-container fill-height fluid>
+                <v-row
+                  align="center"
+                  justify="center"
+                >
+                  <v-col cols="9">
+                    <v-text-field type="email" v-model="email" name="email" placeholder="email" />
+                  </v-col>
+                </v-row>
+                <v-row
+                  align="center"
+                  justify="center"
+                >
+                  <v-col cols="9">
+                    <v-text-field type="password" v-model="password" name="password" placeholder="password" />
+                  </v-col>
+                </v-row>
+                  <br>
+                  <div class="error" v-html="error"></div>
+                  <br>
+                  <v-btn @click="register">Register</v-btn>
+              </v-container>
+            </div>
+          </v-col>
+        </v-row>
+      </v-container>
+  </v-layout>
 </template>
 
 <script>
